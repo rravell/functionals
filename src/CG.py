7f1605a1d188c04a7c5fef11229266fe451d5f80
rravell <46346189+rravell@users.noreply.github.com>
2019-04-24 15:44:01 +0200

    Corrected

--- src/CG.py
+++ src/CG.py
@@ -17,15 +17,16 @@ for i in range(0,size[0]):
     for j in range(N**2,size[1]):
         GroupNumber=j%N
         PositionNumber=GroupNumber*N*K**2 
-        if j<=N**2+(N-1):
+        if j<N**2+N: #ESTOS SON LOS MARGINALES DE ALICE
             newfunctionals[i][PositionNumber]=functionals[i][j]
             newfunctionals[i][PositionNumber+1]=functionals[i][j]
+
         
-        else:                
+        else: #ESTOS LOS DE BOB             
             newfunctionals[i][N*GroupNumber]+=functionals[i][j]
             newfunctionals[i][N*GroupNumber+2]=functionals[i][j]
         
-for i in range(0,size[0]):
+for i in range(0,size[0]): #SUMO LOS 16 PRIMEROS TERMINOS I.E. P(00|XY)
     for j in range(0,N**2):
         newfunctionals[i][N*j]+=functionals[i][j]       
         
